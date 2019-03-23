5bbb6525ccd60724c9c1b74155e268bd37ff1672
zodiacfly <renzodiac@hotmail.com>
2019-03-23 13:03:12 +1100

    use __dirname

--- crud-mongodb-mongoose/nodeApp.js
+++ crud-mongodb-mongoose/nodeApp.js
@@ -2,10 +2,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var router = require('./router.js');
+var path = require('path');
 var app = express();
 
-app.use('/public/',express.static('./public/'));
-app.use('/node_modules/',express.static('./node_modules/'));
+app.use('/public/',express.static(path.join(__dirname,'./public/')));
+app.use('/node_modules/',express.static(path.join(__dirname,'./node_modules/')));
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
